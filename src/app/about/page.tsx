7fb41358d8e932786f0b4cefe2fd9c1d2ae4d426
Haashir Nawaz <153224457+haashirnawaz9@users.noreply.github.com>
2025-09-13 13:40:49 -0400

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -21,6 +21,7 @@ const fadeIn = {
   },
 }
 
+
 const DocumentationPage = () => {
   return (
     <div className="relative min-h-screen bg-gradient-to-b from-[#0f172a] via-[#162033] to-[#1e293b] text-white overflow-hidden">
